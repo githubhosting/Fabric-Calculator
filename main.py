3322908b4fcd9dbf6285fe090747fbac80dce7a1
myselfshravan <myselfshravan@gmail.com>
2023-10-04 00:37:10 +0530

    config added

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import streamlit as st
 import math
 
-st.set_page_config(page_title="Fabric Calculator", page_icon="")
+st.set_page_config(page_title="Fabric Calculator", page_icon=":calculator")
 
 st.title("Fabric Calculator")
 
 width = st.number_input("Enter the Window Width :", step=1)
 height = st.number_input("Enter the Window Height :", step=1)
-panel=math.floor(width/20)
-mam=round((height+10)/39+0.5,1)
-shm=round((height+12)/39+0.5,1)
+panel = math.floor(width / 20)
+mam = round((height + 10) / 39 + 0.5, 1)
+shm = round((height + 12) / 39 + 0.5, 1)
 st.subheader(f"No of Panels : {panel}")
 st.subheader(f"Main Material : {mam}")
 st.subheader(f"Shear Material : {shm}")
 
 st.write(" ")
-st.subheader(f"Total Main Material : {panel*mam}")
-st.subheader(f"Total Shear Material : {panel*shm}")
+st.subheader(f"Total Main Material : {panel * mam}")
+st.subheader(f"Total Shear Material : {panel * shm}")
 
 hide_streamlit_style = """
                     <style>
