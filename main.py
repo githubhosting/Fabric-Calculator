1706c78e0c0399b9e7150210cc736047f54fa3e5
myselfshravan <myselfshravan@gmail.com>
2023-12-05 21:46:33 +0530

    slide removed

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ name, authentication_status, username = authenticator.login('Login', 'main')
 
 def home():
     st.title("Fabric Calculator")
-    input_type = st.radio("Select Input Type", ("Number", "Slider"), horizontal=True)
-
-    if input_type == "Slider":
-        width = st.slider("Enter the Window Width :", min_value=1, max_value=100, step=1)
-        height = st.slider("Enter the Window Height :", min_value=1, max_value=100, step=1)
-    else:
-        width = st.number_input("Enter the Window Width :", min_value=1, step=1)
-        height = st.number_input("Enter the Window Height :", min_value=1, step=1)
+    # input_type = st.radio("Select Input Type", ("Number", "Slider"), horizontal=True)
+    #
+    # if input_type == "Slider":
+    #     width = st.slider("Enter the Window Width :", min_value=1, max_value=100, step=1)
+    #     height = st.slider("Enter the Window Height :", min_value=1, max_value=100, step=1)
+    # else:
+    width = st.number_input("Enter the Window Width :", min_value=1, step=1)
+    height = st.number_input("Enter the Window Height :", min_value=1, step=1)
     panel = math.floor(width / 20)
     mam = (height + 10) / 39 + 0.5
     shm = (height + 12) / 39 + 0.5
